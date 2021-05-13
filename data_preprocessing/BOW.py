5ba1809b7cead56117271ddbe7b635d5a3baa797
Hector <hectora7@gmail.com>
2021-05-13 16:01:31 +0200

    Full size vocabulary, fixed max sentence length problem. Deleted BOW.py from parent directory

--- data_preprocessing/BOW.py
+++ data_preprocessing/BOW.py
@@ -1,11 +1,13 @@
 import numpy as np
 import csv
 
+
 class PadMaxLength:
 
     def __init__(self, file_name):
         self.file = open(file_name)
         self.csv_file = csv.DictReader(self.file)
+        self.list_padded_sentences = []
         self.text = []
         for col in self.csv_file:
             self.text.append(col["text"])
@@ -14,7 +16,7 @@ class PadMaxLength:
         tokenized_sentences = []
         # split each sentence into words
         for sentence in self.text:
-            tokens = sentence.split(" ")
+            tokens = sentence.split()
             tokenized_sentences.append(tokens)
         # get longest sentence and its length
         longest_sent = max(tokenized_sentences, key=len)
@@ -24,41 +26,49 @@ class PadMaxLength:
         shortest_sent = min(tokenized_sentences, key=len)
         shortest_sent_len = len(shortest_sent)
 
-        return (longest_sent_len, shortest_sent_len)
+        return longest_sent_len, shortest_sent_len
 
     def right_pad_sentences(self, max_sent_length):
-        max_len = round(max_sent_length * 0.50) # Take 50% of the maximum sentence length to avoid sparsity
+        max_len = round(max_sent_length * 0.50)  # Take 50% of the maximum sentence length to avoid sparsity
         padded_sentences = []
-        list_padded_sentences = []
+        # print(max_len)
 
         for sentence in self.text:
-            sent = sentence.strip()
-            sent = sent.split(" ")
+            sentence = sentence.strip()
+            sentence = sentence.split()
 
-            if len(sent) > max_len:
-                a = sent[:max_len] # discard tokens longer than max_length
+            if len(sentence) > max_len:
+                a = sentence[:max_len]  # discard tokens longer than max_length
                 padded_sentences.append(a)
 
-            elif len(sent) < max_len:
-                [sent.append("0") for i in range(max_len - len(sent))] # pad sentences with zeros smaller than max_length
-                padded_sentences.append(sent)
+            elif len(sentence) < max_len:
+                [sentence.append("0") for i in
+                 range(max_len - len(sentence))]  # pad sentences with zeros smaller than max_length
+                padded_sentences.append(sentence)
 
             else:
-                padded_sentences.append(sent)
+                padded_sentences.append(sentence)
 
         for pad_sent in padded_sentences:
             list_sentences = ' '.join(pad_sent)
-            list_padded_sentences.append(list_sentences)
-        return list_padded_sentences
+            self.list_padded_sentences.append(list_sentences)
+
+        return self.list_padded_sentences
+
+    def merge_with(self, list2, list3):
+        merged = self.list_padded_sentences + list2 + list3
+        return merged
+
 
 class BagOfWords:
 
-    def __init__(self, list_of_sentences):
+    def __init__(self, all_padded_sentences, list_of_sentences):
 
         # define punctuation and upper case alphabet
         self.punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         self.upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        self.vocab = self.generate_vocabulary(list_of_sentences)  # Generate the vocabulary
+        self.vocab = self.generate_vocabulary(all_padded_sentences)  # Generate the vocabulary
+        #print(len(self.vocab))
         self.dict_idx = self.indexing(self.vocab)  # Generate the indexing
         self.word_count = self.count_dictionary(list_of_sentences)
         self.N_sentences = len(list_of_sentences)
@@ -75,12 +85,12 @@ class BagOfWords:
             else:
                 lowercase = lowercase + char
         lowercase = lowercase.strip()
-        tokenized = list(lowercase.split(" "))
+        tokenized = list(lowercase.split())
         return tokenized
 
-    def generate_vocabulary(self, list_of_sentences):
+    def generate_vocabulary(self, all_padded_sentences):
         vocab = []
-        for sentence in list_of_sentences:
+        for sentence in all_padded_sentences:
             tokenized_sentence = self.lowercase_tokenize(sentence)
             for word in tokenized_sentence:  # append only unique words
                 if word not in vocab:
@@ -100,29 +110,27 @@ class BagOfWords:
         word_count = {}
         for word in self.vocab:
             word_count[word] = 0.0
-            for sent in input_sentences:
-                if word in sent:
+            for sentence in input_sentences:
+                if word in sentence:
                     word_count[word] += 1.0
         return word_count
 
     # Term Frequency
     def termfreq(self, sentence, word):
-        N_sentence = float(len(sentence))
+        number_of_sentences = float(len(sentence))
         occurrence = float(len([token for token in sentence if token == word]))
-        return occurrence / N_sentence
+        return occurrence / number_of_sentences
 
     def inverse_doc_freq(self, word):
         try:
             word_occurrence = self.word_count[word] + 1.0
-        except:
+        except KeyError:
             word_occurrence = 1.0
         return np.log(self.N_sentences / word_occurrence)
 
     def tf_idf(self, input_sentences):
         row = 0
         tf_idf_vec = np.zeros((self.N_sentences, (len(self.vocab))))
-
-        #tf_idf_vec = np.zeros(((len(self.vocab),)))
 
         for sentence in input_sentences:
             tokenized_sentence = self.lowercase_tokenize(sentence)
@@ -133,7 +141,6 @@ class BagOfWords:
                 value = tf * idf
                 tf_idf_vec[row][self.dict_idx[word]] = value
 
-                #tf_idf_vec[self.dict_idx[word]] = value
             row += 1
 
         return tf_idf_vec
@@ -149,9 +156,10 @@ class BagOfWords:
     #         row += 1
     #     return bow_vector
 
-train_file = "/home/lara/PycharmProjects/pythonProject/CLab21/data/emotions/isear/isear-train-modified.csv"
-val_file = "/home/lara/PycharmProjects/pythonProject/CLab21/data/emotions/isear/isear-val-modified.csv"
-test_file = "/home/lara/PycharmProjects/pythonProject/CLab21/data/emotions/isear/isear-test-modified.csv"
+
+train_file = "../data/emotions/isear/isear-train-modified.csv"
+val_file = "../data/emotions/isear/isear-val-modified.csv"
+test_file = "../data/emotions/isear/isear-test-modified.csv"
 
 pml_train = PadMaxLength(train_file)
 pml_val = PadMaxLength(val_file)
@@ -160,23 +168,32 @@ pml_test = PadMaxLength(test_file)
 max_sent, min_sent = pml_train.min_max_sentences()
 
 sentences_padded_train = pml_train.right_pad_sentences(max_sent)
-sentences_padded_val = pml_val.right_pad_sentences(max_sent)
-sentences_padded_test = pml_test.right_pad_sentences(max_sent)
+# print("Len first sentence of train File", len(sentences_padded_train[0].split()))
 
-bow_train = BagOfWords(sentences_padded_train)  # Sentences to create the vocabulary
-bow_val = BagOfWords(sentences_padded_val)
-bow_test = BagOfWords(sentences_padded_test)
+sentences_padded_val = pml_val.right_pad_sentences(max_sent)
+# print("Len first sentence of val File", len(sentences_padded_val[0].split()))
+
+sentences_padded_test = pml_test.right_pad_sentences(max_sent)
+# print("Len first sentence of test File", len(sentences_padded_test[0].split()))
+
+vocab_list = pml_train.merge_with(sentences_padded_val, sentences_padded_test)  # Vocab over all files
+
+bow_train = BagOfWords(vocab_list, sentences_padded_train)  # Sentences to create the vocabulary
+bow_val = BagOfWords(vocab_list, sentences_padded_val)
+bow_test = BagOfWords(vocab_list, sentences_padded_test)
 
 tf_idf_train = bow_train.tf_idf(sentences_padded_train)
 tf_idf_val = bow_val.tf_idf(sentences_padded_val)
 tf_idf_test = bow_test.tf_idf(sentences_padded_test)
 
 print("tf_idf_train \n", tf_idf_train.shape)
+print(tf_idf_train)
 print("\n")
 print("tf_idf_val \n", tf_idf_val.shape)
+print(tf_idf_val)
 print("\n")
 print("tf_idf_test \n", tf_idf_test.shape)
-
+print(tf_idf_test)
 
 
 # vector_train = bow_train.bag_of_words(sentences_padded_train)  # BOW over some other sentences
