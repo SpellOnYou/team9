3e424724008a3aa0b7f9fed4f3c5226a8cc557b9
Lara Grimminger <laragrimminger@live.de>
2021-07-06 23:18:59 +0200

    Csv files with columns label, text, osp and tense as well as lstm_awd script and main method for lstm_awd

--- scripts/lstm_awd.py
+++ scripts/lstm_awd.py
@@ -1,84 +1,99 @@
 # -*- coding: utf-8 -*-
-# Author: Jiwon Kim
+# Author: Jiwon Kim, Lara Grimminger
 
-# before executing this code, if you don't have fastai library, I recommend you to download it first using pip install pip install fastai
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 from fastai.text.all import *
 from collections import Counter
 import pandas as pd
-
-def get_data(path):
-    '''get data with given path.
-    Be careful since some labels (e.g. disgust) have mixed cases (upper, lower)
-    '''
-    with path.open() as f:
-        for line_i, line in enumerate(f):
-            if line_i == 0: continue
-            label, text = line.split(',', maxsplit=1)
-            yield label.lower(), text.strip('\n')train_data = get_data(train_path)
+from sklearn.metrics import classification_report
 
 
-def get_df(train_path, valid_path, test_path):
-    '''
-    this function gets train, valid, test path and return train, valid data to data frame format.
-    '''
+class LstmAwd:
 
-    train_data, valid_data, test_data = map(get_data, (train_path, valid_path, test_path))
-    train_df, valid_df, test_df = map(pd.DataFrame, (train_data, valid_data, test_data))
-    
-    train_df['is_valid'], valid_df['is_valid'] = False, True    
-    
-    train_df = train_df.append(valid_df)
-
-    return train_df, test_df
-
-def train(train_df):
-    '''
-    This function composed of mainly 2 phases.
-    1. train 2. interpretation.
-    you can customize this fuction as you need.
-
-    In a phase of training, model is initialiezd using AWD_LSTM architecture, with pre-trained language model (with wikipedia) using fastai API.
-    more specifically,
-        First, it makes data loader,
-        Second, it defines model finetune shallow epochs
-        Third, with given learning rate
-            learning rate is given by learning rate finder, which finds out steepest point of error curve.
-        
-    After training, model shows interpretation of its result.
-        First, case most confused, actual, predict, number of cases from leftmost.
-        Second, it shows most highest loss and the text (loss function is cross entropy)
-        Finally, plot confusion matrix
-    '''
-
-    dls = TextDataLoaders.from_df(train_df, path='.', text_col = 1, label_col = 0, valid_col='is_valid')
-    learn = text_classifier_learner(dls, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
-    learn.fine_tune(4, 1e-2)
-    lr_new = learn.lr_find().valley
-    learn.fine_tune(10, lr_new)
-
-    interp = ClassificationInterpretation.from_learner(learn)
-    interp.most_confused(min_val=3)
-    interp.plot_top_losses(10, figsize=(15,15))
-    interp.plot_top_losses(10, figsize=(15,15))
-
-    return learn
+    def get_data(self, path):
+        """
+        get data with given path.
+        Be careful since some labels (e.g. disgust) have mixed cases (upper, lower)
+        """
+        with path.open() as f:
+            for line_i, line in enumerate(f):
+                if line_i == 0: continue
+                label, text, osp, tense = line.split(',', maxsplit=3)
+                # label, text = line.split(',', maxsplit=1)
+                yield label.lower(), text.strip('\n'), osp.strip('\n'), tense.strip('\n')
+                # yield label.lower(), text.strip('\n')
 
 
-# customize to your code
-data_path = Path('/content/CLab21/datasets/emotions/isear')
-train_path = data_path / 'isear-train-modified.csv'
-valid_path = data_path / 'isear-val-modified.csv'
-test_path = data_path / 'isear-test-modified.csv'
+    def get_df(self, train_path, valid_path, test_path):
+        """
+        this function gets train, valid, test path and return train, valid data to data frame format.
+        """
 
-train_df, test_df = get_df(train_path, valid_path, test_path)
-model = train(train_df)
+        train_data, valid_data, test_data = map(self.get_data, (train_path, valid_path, test_path))
+        train_df, valid_df, test_df = map(pd.DataFrame, (train_data, valid_data, test_data))
+        train_df['is_valid'], valid_df['is_valid'] = False, True
+        train_df = train_df.append(valid_df)
+
+        return train_df, test_df
+
+    def train(self, train_df, textCol, file_name):
+        """
+        This function composed of mainly 2 phases.
+        1. train 2. interpretation.
+        you can customize this function as you need.
+
+        In a phase of training, model is initialiezd using AWD_LSTM architecture, with pre-trained language model
+        (with wikipedia) using fastai API.
+        more specifically,
+            First, it makes data loader,
+            Second, it defines model finetune shallow epochs
+            Third, with given learning rate
+                learning rate is given by learning rate finder, which finds out steepest point of error curve.
+
+        After training, model shows interpretation of its result.
+            First, case most confused, actual, predict, number of cases from leftmost.
+            Second, it shows most highest loss and the text (loss function is cross entropy)
+            Finally, plot confusion matrix
+        """
+
+        dls = TextDataLoaders.from_df(train_df, path='.', text_col=textCol, label_col=0, valid_col='is_valid')
+        learn = text_classifier_learner(dls, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
+        learn.fine_tune(4, 1e-2)
+        lr_new = learn.lr_find().valley
+        learn.fine_tune(10, lr_new)
+
+        interp = ClassificationInterpretation.from_learner(learn)
+        interp.most_confused(min_val=3)
+        interp.plot_top_losses(10, figsize=(15, 15))
+        interp.plot_confusion_matrix()
+
+        plt.savefig(file_name)
+
+        return learn
+
+    def test(self, learn, test_path, columns):
+
+        test_df = pd.read_csv(test_path)
+        test_features = test_df[columns]
+        test_features = test_features.values.tolist()
+
+        ref_dict = {'anger': 1, 'disgust': 2, 'fear': 3, 'guilt': 4, 'joy': 5, 'sadness': 6, 'shame': 7}
+        yhat_list = []
+
+        for text in test_features:
+            yhat_list.append(learn.predict(text)[1].item())
+
+        get_idx = lambda x: ref_dict[x]
+        ytrue_list = list(map(get_idx, test_df['label'].tolist()))
+
+        print(classification_report(ytrue_list, yhat_list, target_names=ref_dict.keys()))
 
 '''
 you can predict your test data using model.predict (as sklearn.)
 
-e.g. model.predit('When I understood that I was admitted to the University.') will asnwer you
+e.g. model.predict('When I understood that I was admitted to the University.') will answer you
 
 >>> ('joy',
  tensor(5),
@@ -88,5 +103,3 @@ e.g. model.predit('When I understood that I was admitted to the University.') wi
 
 that means you have answer 'joy' corresponding to tensor 5 (among 0-6), with '9.9196e-01' probability (which is 99%)
 '''
-
-
---
+++ scripts/main_lstm_awd.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from lstm_awd import LstmAwd
+
+
+class MainLSTM:
+
+    def __init__(self, **kwargs):
+
+        self.kwargs = {k: v for k, v in kwargs.items()}
+        self.is_trace = self.kwargs['trace'] if 'trace' in self.kwargs else False
+
+        self.file_names = ["LSTM_AWD_Text_confusion_matrix.png",
+                           "LSTM_AWD_OCC_confusion_matrix.png",
+                           "LSTM_AWD_Text_OCC_confusion_matrix.png"]
+
+    def __call__(self):
+
+        if self.is_trace:
+            print("Loading dataset....")
+        self.get_path()
+        self.lstm_model = LstmAwd()
+        self.train_lstm()
+
+    def get_path(self):
+
+        """Get data path, as we wrote down in self._get_user_path, this isn't currently user-interactive.
+        """
+
+        if 'root_data' in self.kwargs:
+            self._get_user_path(self.kwargs['root_path'])
+
+        else:
+            self.data_path = Path('../datasets/emotions/isear')
+            self.train_path = self.data_path / 'train_occ.csv'
+            self.valid_path = self.data_path / 'val_occ.csv'
+            self.test_path = self.data_path / 'test_occ.csv'
+
+    def train_lstm(self):
+
+        train_df, test_df = self.lstm_model.get_df(self.train_path, self.valid_path, self.test_path)
+
+        # LSTM_AWD with feature text
+        model = self.lstm_model.train(train_df, 1, self.file_names[0])
+        self.lstm_model.test(model, self.test_path, ["text"])
+
+        # LSTM_AWD with feature OCC variables
+        model = self.lstm_model.train(train_df, [2, 3], self.file_names[1])
+        self.lstm_model.test(model, self.test_path, ["osp", "tense"])
+
+        # LSTM_AWD with features text and OCC variables
+        model = self.lstm_model.train(train_df, [1, 2, 3], self.file_names[2])
+        self.lstm_model.test(model, self.test_path, ["text", "osp", "tense"])
+
+if __name__ == "__main__":
+    lstm_model = MainLSTM()
+    lstm_model()
