7fbf8aa5d9e3d5b9435b1c24a5103b2666f2c132
KIMJIWON08 <ling.cse.jw@gmail.com>
2021-07-28 12:01:02 +0900

    start adding mlp script

--- team9/classify.py
+++ team9/classify.py
@@ -17,32 +17,6 @@ class Classifier():
 		self.occ_type = occ_type
 		self.kwargs = {k: v for k, v in kwargs.items()}
 
-
-	# def get_data(self):
-	# 	"""
-	# 	Description:
-	# 		here pkgname is module where data exists.
-
-	# 	"""
-	# 	self.x_train_text, self.y_train_label = load_csv(fpath = 'example/train', occ_type = self.occ_type)
-	# 	self.x_valid_text, self.y_valid_label = load_csv(fpath= 'example/valid', occ_type = self.occ_type)
-	# 	self.x_test_text, self.y_test_label = load_csv(fpath='example/test', occ_type = self.occ_type)
- #        # TODO: getting occ variable
-	# 	# change when labels aren't int
-	# 	self.label2idx, self.y_train = validate_y(self.y_train_label)
-	# 	self.y_valid = list(map(lambda x: self.label2idx[x], self.y_valid_label))
-	# 	self.y_test = list(map(lambda x: self.label2idx[x], self.y_test_label))
-
-	# def get_embedding(self):
-	# 	#TODO: it's better to divide fit / transform of tfidf and override function with additional class,
-	# 	# so that both type of embedding can be obtained same method
-	# 	if self.emb_type=='tfidf':
-	# 		self.embedding = emb_dict[self.emb_type]
-	# 		self.x_train = self.embedding.fit_transform(self.x_train_text)
-	# 	else:
-	# 		self.embedding = load_npz(self.emb_type)
-	# 		self.vtoi = load_pkl(self.emb_type)
-
 	def train(self, x, y):
 		self.model.fit(x, y)
 		train_pred = self.model.predict(x)
--- team9/data/databunch.py
+++ team9/data/databunch.py
@@ -14,7 +14,10 @@ import numpy as np
 emb_type = ['fasttext','tfidf']
 
 class DataBunch():
-	"""load all data and convert it to appropriate embedding type. embedding type will be obtrained in main(classify) module"""
+	"""load all data and convert it to appropriate embedding type. embedding type will be obtrained in main(classify) module
+	Note: when you load embedding from pre-trained model, it will have tensor size (dataset, max_length, emb_dim) which can't feed on MLP/NB.
+		It will be flattend (i.e. tensor size (dataset, max_length * emb_dim)) unless other option is given.
+	"""
 	def get_embedding(self, **kwargs):
 		#TODO: it's better to divide fit / transform of tfidf and override function with additional class,
 		# so that both type of embedding can be obtained same method
@@ -35,8 +38,12 @@ class DataBunch():
 
 	def get_data(self):
 		"""
-		text(list(str))
-		label(list(str))
+		A function which loads dataset with given (or default) file path
+
+		Attribute
+		x_train_text: (list(one data))
+			list of text data, and each sentence(or document) is not tokenized
+		y_test_label: (list(one data))
 		"""
 		#TODO:we can get dir path's data at once and split by ourselves
 
@@ -73,13 +80,9 @@ class DataBunch():
 
 		#padding, vacant words will be substituted as unknown
 		if not 'max_seq' in kwargs: max_seq = max([len(i) for i in tokens_in_sents])
-		pad_fn = lambda sent: sent + ['-1'] * (max_seq-len(sent))
+		pad_fn = lambda sent: sent + [-1] * (max_seq-len(sent))
 		self.pad_num_sents = list(map(pad_fn, text_to_nums))
 		
 		#third process
 		pre_trained = load_npz(self.emb_type)
 		return pre_trained
-
-	# def __getitem__(self, idx):
-	# 	return self.x[idx], self.y[idx]
-
