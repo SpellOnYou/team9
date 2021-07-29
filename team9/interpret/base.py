17c77df5686358571f87130a8034a843cd617aa6
Lara Grimminger <laragrimminger@live.de>
2021-07-29 12:24:03 +0200

    added lime in base.py

--- team9/interpret/base.py
+++ team9/interpret/base.py
@@ -8,7 +8,8 @@ import seaborn as sn
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def cm(true_label, pred_label, labels=None, fname = 'confusion_matrix', **kwargs):
+
+def cm(true_label, pred_label, labels=None, fname='confusion_matrix', **kwargs):
     """
     The function not only calculate confusion matrix, but also visualize (i.e. sys out write) as heatmap
 
@@ -20,19 +21,37 @@ def cm(true_label, pred_label, labels=None, fname = 'confusion_matrix', **kwargs
         Predictoin from target data
     labels: array-like of shape (n_classes), default=None
         labels which index confusion matrix
-    
+
     Note
     ----------
     Please be sure that input should be 1-D
 
     """
     cm_score = confusion_matrix(true_label, pred_label)
-    df_cm = pd.DataFrame(cm_score, index = labels, columns = labels)
-    plt.figure(figsize = (10,7))
+    df_cm = pd.DataFrame(cm_score, index=labels, columns=labels)
+    plt.figure(figsize=(10, 7))
     sn.heatmap(df_cm, annot=True, **kwargs)
-    img_path = Path(__package__.split('.')[0])/'plot'
-    img_path.mkdir(exist_ok=True, parents = True)
-    plt.savefig(img_path/fname)
+    img_path = Path(__package__.split('.')[0]) / 'plot'
+    img_path.mkdir(exist_ok=True, parents=True)
+    plt.savefig(img_path / fname)
     print(f"Your confusion matrix is saved at : {str(img_path)} named {fname}")
-    
-def lime(): pass
+
+
+def lime_predictor(self, text):
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit_transform(x_data)
+    text_vector = vectorizer.transform(text).toarray()
+    prob = model.pre  # x_datadict(text_vector, batch_size=64, verbose=1)
+    return prob
+
+
+def lime(x_data, y_data):
+    idx = 906
+    target_names = ["Anger", "Disgust", "Fear", "Guilt", "Joy", "Sadness", "Shame"]
+    explainer = LimeTextExplainer(class_names=target_names)
+    row = y_data[idx]
+    print("Row: %s" % row)
+    exp = explainer.explain_instance(row, lime_predictor, num_features=7, top_labels=7)
+    exp.save_to_file(f"results_Lime/ISEAR_{idx}_occ_rule.html")
+
+    return exp
